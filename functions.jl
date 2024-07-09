12873ccc2c626c0e4b7214bc5bd1d88750e0912b
Carlos Payá <carlos.paya@csic.es>
2024-07-09 17:02:33 +0200

    add infinite loop

--- functions.jl
+++ functions.jl
@@ -3,7 +3,12 @@ function work_load(; rng = range(0, 100, 1000))
     pts = Iterators.product(rng, rng)
     A = pmap(pts) do pt
         x, y = pt 
-        return inv(x*B() + y*B())
+        i = 1
+        C = B()
+        while i > 0
+            C = C * B()
+        end
+        return C
     end
     return A
 end
